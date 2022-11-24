71fa84a80a19a127d672dc507133edfc9de26f13
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-24 19:59:55 -0300

    Adicionando Projeto 6 - Atribuições de variáveis

---
+++ 6-AtribuicaoDeVariaveis/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 6 - Atribuições de variáveis");
+
+        int idade = 25;
+        int novaIdade = idade;
+
+        Console.WriteLine($"Idade: {novaIdade}");
+
+        idade = 30;
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
