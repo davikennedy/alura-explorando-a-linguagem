73c10636b7c4d6bee209726df3a07bbd33e18323
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-24 19:24:21 -0300

    Adicionando Projeto 5-CaracteresETextos

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra); // Saída: a
+
+        letra = (char)65;
+        Console.WriteLine(letra); // Saída: A
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra); // Saída: B
+
+        Console.ReadLine();
+    }
+}
