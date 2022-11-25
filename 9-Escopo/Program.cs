cbe48b28837572685aab7a6946bf011a8b43620b
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-25 15:37:52 -0300

    Adicionando Projeto 9 - Escopo

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 9 - Escopo");
+
+        int idade = 16;
+        int quantidadePessoas = 2;
+
+        bool maiorDeIdade = idade >= 18;
+        bool acompanhado = quantidadePessoas > 1;
+
+        // Essa variável precisa ser declarada fora do escopos das condicionais 'if'
+        string textoAdicional;
+
+        if (acompanhado)
+        {
+            textoAdicional = "João está acompanhado";
+        }
+        else
+        {
+            textoAdicional = "João não está acompanhado";
+        }
+
+        if (maiorDeIdade || acompanhado)
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
