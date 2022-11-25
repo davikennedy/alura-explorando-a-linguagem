5683f43a448540ab87829d1fc0f37f4b495b0ee0
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-24 23:06:28 -0300

    Adicionando Projeto 8 - Condicionais 2

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 8 - Condicionais 2");
+
+        int idade = 16;
+        int quantidadePessoas = 2;
+
+        bool maiorDeIdade = idade >= 18;
+        bool acompanhado = quantidadePessoas > 1;
+
+        if (maiorDeIdade || acompanhado)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
