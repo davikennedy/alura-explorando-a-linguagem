f3010dda8645d07f2c9c9996e497498abce08d5a
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-26 13:57:09 -0300

    Adicionado Projeto 10 - Calcula Poupança

---
+++ 10-CalculaPoupanca/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 10 - Calcula Poupança");
+
+        Console.Write("Valor a ser investido: ");
+        double investimento = double.Parse(Console.ReadLine());
+
+        Console.Write("Em quantos meses pretende resgatar o valor? ");
+        int quantidadeMeses = int.Parse(Console.ReadLine());
+
+        int mes = 1;
+        while (mes <= quantidadeMeses)
+        {
+            investimento = investimento + (investimento * 0.005);
+            Console.WriteLine(investimento);
+            mes++;
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
