9c07a0cb6d8ad005ceb70b5d2a96b205f6aa2770
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-26 15:06:20 -0300

    Adicionando Projeto 13 - Encadeando For

---
+++ 13-EncadeandoFor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Encadeando For");
+
+        for (int contadorLinhas = 1; contadorLinhas <= 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 1; contadorColunas <= 10; contadorColunas++)
+            {
+                Console.Write("*");
+                if (contadorLinhas == contadorColunas)
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
