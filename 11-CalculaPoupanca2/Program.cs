7bc7985022a15f38103e6b2f9922f8a9f777ff44
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-26 14:20:21 -0300

    Adicionando Projeto 11 - Calcula Poupança 2

---
+++ 11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 11 - Calcula Poupança 2");
+
+        Console.Write("Valor a ser investido: ");
+        double investimento = double.Parse(Console.ReadLine());
+
+        Console.Write("Em quantos meses pretende resgatar o valor? ");
+        int quantidadeMeses = int.Parse(Console.ReadLine());
+
+        for (int mes = 1; mes <= quantidadeMeses; mes++)
+        {
+            investimento *= 1.005;
+            Console.WriteLine($"Valor no {mes}º mês: R$ {(Math.Round(investimento, 2))}");
+        }
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
