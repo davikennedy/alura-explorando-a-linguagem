ef52fd7f76f7559403337dfa7ddd77edb5073369
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-26 13:56:41 -0300

    Renomeando projetos

---
+++ 20-ResolvendoDesafio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.WriteLine("Escreva uma letra maiúscula: ");
+
+string input = Console.ReadLine();
+char inputChar = char.Parse(input);
+char letra = ' ';
+
+for (int i = 65; i <= 90; i++)
+{
+    letra = (char)i;
+    if (inputChar == letra)
+    {
+        Console.WriteLine("Você escreveu uma letra maiúscula");
+    }
+    else
+    {
+        Console.WriteLine("Você escreveu uma caracter inválido");
+    }
+}
+
+Console.WriteLine($"Você escreveu {input}");
