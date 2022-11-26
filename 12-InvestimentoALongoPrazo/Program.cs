2ad23c1d37cb71177299dfe29c678c550d9f6624
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-26 14:37:43 -0300

    Adicionando Projeto 12 - Investimento à longo prazo

---
+++ 12-InvestimentoALongoPrazo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 12 - Investimento à longo prazo");
+
+        Console.Write("Valor a ser investido: ");
+        double investimento = double.Parse(Console.ReadLine());
+
+        Console.Write("Em quantos anos pretende resgatar o valor? ");
+        int quantidadeAnos = int.Parse(Console.ReadLine());
+
+        double fatorRendimento = 1.005;
+
+        for (int ano = 1; ano <= quantidadeAnos; ano++)
+        {
+            for (int mes = 1; mes <= 12; mes++)
+            {
+                investimento *= fatorRendimento;
+            }
+            fatorRendimento += 0.001;
+        }
+
+        Console.WriteLine($"\nDepois de {quantidadeAnos} anos, você terá: R$ {(Math.Round(investimento, 2))}");
+
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
